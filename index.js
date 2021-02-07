ef38f9517646a0740001fee5401bea31d9a40052
Mateusz Bien <mateuszbien@yahoo.com>
2021-02-07 17:42:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// console.log($);
+// console.log(jQuery);
+
+// $('h1').text('Hello Word');
+
+// $('body').css('margin', 0).css('min-height', '100vh');
+
+$('body').css({
+    margin:0,
+    minHeight: '100vh',
+});
+
+// $('p:first').css({
+//     color: 'red',
+// })
+
+// $('p:even').css({
+//     color: 'red',
+// })
+
+// $('p:odd').css({
+//     color: 'red',
+// })
+
+// $('p:gt(5)').css({
+//     color: 'red',
+// })
+
+// $('p:lt(5)').css({
+//     color: 'red',
+// })
+
+// $('p:eq(5)').css({
+//     color: 'red',
+// })
+
+// $('p:not(.example-class)').css('color', 'red',);
+
+// $('p:not(.example-class)').css('color', 'red',);
+
+// $('p[data-example=example]').css('color', 'red',);
+
+// $('p[data-example^=example]').css('color', 'red',);
+
+// $('.anime')
+// .css({
+//     width: 200,
+//     height: 200,
+//     backgroundColor: "red",
+//     bottom: 0,
+//     left: 0,
+//     position: 'fixed',
+// })
+
+// .animate({
+//     left: 500,
+// }, 3000)
+
+// .animate({
+//     left: 0,
+// }, 3000);
+
+// $('#add-class').click(() => $('.anime').addClass('pink'));
+// $('#remove-class').click(() => $('.anime').removeClass('pink'));
+// $('#toggle-class').click(() => $('.anime').toggleClass('pink'));
+
+// $('#hide').click(() => $('.anime').hide());
+// $('#show').click(() => $('.anime').show());
+
+// $('#slide-in').click(() => $('.anime').slideUp(500));
+// $('#slide-down').click(() => $('.anime').slideDown(500));
+
+// $('body').click(() => $('.anime').text("500"));
+// $('body').click(() => $('h1').append(" moto"));
